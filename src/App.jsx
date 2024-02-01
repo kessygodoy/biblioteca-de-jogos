bfe607365b83b83fcfac07051768ed09aaefc1c0
Kessy Godoy <kessyon@gmail.com>
2024-01-31 22:13:29 -0300

    App fragmentado em componentes separados
    Todo o codigo estava em um componente apenas e agora separado em varios componentes:
    Game.jsx
    NewGameForm.jsx
    TextInput.jsx
    e o hook useGameCollecion

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,27 @@
-import { useState } from "react"
+
+import Game from "./components/Game"
+import NewGameForm from "./components/NewGameForm"
+import useGameCollection from "./hooks/useGameCollection"
 
 export default function App(){
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib")
-    if(!storedGames) return []
-    const gameArray = JSON.parse(storedGames)
-    return gameArray
-  })
-  const [title, setTitle] = useState("")
-  const [cover, setCover] = useState("")
+  const {games, addGame, removeGame} = useGameCollection()
 
-
-  const addGame = ({title, cover}) => {
-    const id = Math.floor(Math.random() * 1000000)
-    const game = {id, title, cover}
-    setGames(state => {
-      const newState = [...state, game]
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-  
-  const removeGame = (id) => {
-    setGames(state => {
-      const newState = state.filter(game => game.id !== id)
-    localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-    return newState
-    })
-  }
-
-  const handleSubmit = (ev) => {
-    ev.preventDefault()
-    addGame({title, cover})
-    console.log(title, cover)
-    setTitle("")
-    setCover("")
-  }
   return(
     <div id="app">
       <h1>Biblioteca de Jogos</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Titulo:</label>
-          <input 
-            type="text" 
-            name="title" 
-            id="title" 
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="cover">Capa:</label>
-          <input 
-            type="text" 
-            name="cover" 
-            id="cover" 
-            value={cover}
-            onChange={(e) => setCover(e.target.value)}
-          />
-        </div>
-        <button type="submit">Adicionar à biblioteca</button>
-      </form>
+      <NewGameForm addGame={addGame} />
       <div className="games">
-        {games.map((game) => (
-          <div key={game.id}>
-            <img src={game.cover} alt="" />
-            <div>
-              <h2>{game.title}</h2>
-              <button onClick={() => removeGame(game.id)}>Remover</button>
-            </div>
-          </div>
-       
-        ))}
+        {games.length > 0 ?
+        games.map((game) => (
+          <Game  
+            key={game.id} 
+            title={game.title} 
+            cover={game.cover} 
+            onRemove={() => removeGame(game.id)}/>
+        ))
+          :
+          <h2>Nada para exibir, tente adicionar alguns jogos...</h2>
+      }
       </div>
     </div>
   )
